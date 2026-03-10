59a1133e7fdbcdea42e20a3782e8ac8cf384ebe8
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-03-10 04:01:39 +0200

    Create H. Welcome to school.cpp

---
+++ Problems./H. Welcome to school.cpp
@@ -0,0 +1,34 @@
+// H. Welcome to school
+// URL: https://codeforces.com/group/Rilx5irOux/contest/636158/problem/H
+// Time: 3/10/2026, 03:51:42 AM
+#include <bits/stdc++.h>
+using namespace std;
+#define int         long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    int a ,b; cin >> a >> b;
+    if (b > a){
+        cout <<"YES"<<"\n";
+    }
+    else {
+        cout <<"NO"<<"\n";
+    }
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
