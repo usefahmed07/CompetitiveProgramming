aaa426902b670789cf723d148c5e7479feb07f5d
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-03-12 15:46:27 +0200

    Create L. Luxor.cpp

---
+++ Problems./L. Luxor.cpp
@@ -0,0 +1,34 @@
+// L. Luxor
+// URL: https://codeforces.com/group/Rilx5irOux/contest/532378/problem/L
+// Time: 3/12/2026, 15:51:42 AM
+#include <bits/stdc++.h>
+using namespace std;
+#define int         long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    int a , b; cin >> a >> b;
+    if (a > b){
+        cout <<"YES"<<"\n";
+    }
+    else {
+        cout <<"NO"<<"\n";
+    }
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
