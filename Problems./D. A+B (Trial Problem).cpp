cc4c626e558f6d59b5cd4da75ba0b9717affcb32
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-03-26 20:32:07 +0200

    Create D. A+B (Trial Problem).cpp

---
+++ Problems./D. A+B (Trial Problem).cpp
@@ -0,0 +1,33 @@
+// D. A+B (Trial Problem)
+// URL: https://codeforces.com/group/n8KAvVSzzc/contest/379450/problem/D
+// Time: 3/26/2026, 17:20:42 AM
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    int tt; cin >> tt;
+    while (tt--){
+        ll a , b; cin >> a >> b;
+        cout <<a+b<<"\n";
+    }
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
