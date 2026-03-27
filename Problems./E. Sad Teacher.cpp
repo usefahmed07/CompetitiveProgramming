89f5fe25d084b6b34cd087ba62215209d8159c62
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-03-27 03:12:06 +0200

    Create E. Sad Teacher.cpp

---
+++ Problems./E. Sad Teacher.cpp
@@ -0,0 +1,30 @@
+// E. Sad Teacher
+// URL: https://codeforces.com/group/REUQSW2k4U/contest/463285/problem/E
+// Time: 3/27/2026, 17:20:42 AM
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    ll a , b; cin >> a >> b;
+    cout <<a+b<<"\n";
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
