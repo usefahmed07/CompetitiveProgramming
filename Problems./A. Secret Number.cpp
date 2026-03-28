9f57085a77cb0af80bc9833f51b7acbd6c29f438
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-03-28 06:28:35 +0200

    Create A. Secret Number.cpp

---
+++ Problems./A. Secret Number.cpp
@@ -0,0 +1,34 @@
+// A. Secret Number
+// URL: https://codeforces.com/group/xkS6MyTkfj/contest/269346/problem/A
+// Time: 3/28/2026, 17:20:42 AM
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    int n; cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << 9;
+    }
+
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
