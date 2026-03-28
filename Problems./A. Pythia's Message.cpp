86f10941bb2044b2dc592dfaf942c88a08ae4a7a
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-03-28 20:43:16 +0200

    Create A. Pythia's Message.cpp

---
+++ Problems./A. Pythia's Message.cpp
@@ -0,0 +1,31 @@
+// A. Pythia's Message
+// URL: https://codeforces.com/group/WToO2BEUSL/contest/612936/problem/A
+// Time: 3/28/2026, 17:20:42 AM
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    string x; cin >> x;
+    cout <<"Thank you"<<" "<<x<<"\n";
+
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
