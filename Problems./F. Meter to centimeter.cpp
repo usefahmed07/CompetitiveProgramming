e86ed196e072e561727bae834b0904c1a9393fc5
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-04-04 08:28:53 +0200

    Create F. Meter to centimeter.cpp

---
+++ Problems./F. Meter to centimeter.cpp
@@ -0,0 +1,30 @@
+// F. Meter to centimeter
+// URL: https://codeforces.com/group/p27JFZvaQt/contest/465925/problem/F
+// Date: 4/4/2026
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    int a; cin >> a;
+    cout <<a<<"00"<<" "<<"cm"<<"\n";
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
