b28ca3176138961f891d4fa9164cd139d9e1d916
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-04-04 09:02:29 +0200

    Add A. Odd One Out solution
    Implement solution for 'Odd One Out' problem from Codeforces.

---
+++ Problems./A. Odd One Out.cpp
@@ -0,0 +1,29 @@
+// A. Odd One Out
+// URL: https://codeforces.com/contest/2214/problem/B
+// Date: 4/4/2026
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    cout <<"C2"<<"\n";
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
