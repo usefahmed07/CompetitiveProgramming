3b47488ef3e7d207df5947c0a205f6c3982b94ce
usefahmed07 <youssef.ahmed.hamaza@gmail.com>
2026-04-08 10:50:29 +0200

    Create A. Meow.cpp

---
+++ Problems./A. Meow.cpp
@@ -0,0 +1,31 @@
+// A. Meow
+// URL: https://codeforces.com/group/6OEUPzk8Ai/contest/273293/problem/A
+// Date: 4/8/2026
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    string x; cin >> x;
+    cout <<x<<"eow"<<"\n";
+    
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
