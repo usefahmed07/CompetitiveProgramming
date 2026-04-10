2e4ce4a4fcf68c6e0fb036c5e5ec0ffd2df30c37
usefahmed07 <youssef.ahmed.hamaza@gmail.com>
2026-04-10 20:57:41 +0200

    Create A. How many?.cpp

---
+++ Problems./A. How many?.cpp
@@ -0,0 +1,30 @@
+// A. How many?
+// URL: https://codeforces.com/gym/106179/problem/A
+// Date: 4/10/2026
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    int n; cin >> n;
+    cout <<n*3<<"\n";
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
