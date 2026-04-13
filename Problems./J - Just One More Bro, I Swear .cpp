011e2d243f5ec211d0e8773d4ba3f762a22a9c92
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-04-13 18:09:30 +0200

    Create  	J - Just One More Bro, I Swear .cpp

---
+++ Problems./J - Just One More Bro, I Swear .cpp
@@ -0,0 +1,31 @@
+// BEGIN: main.cpp
+// J. Just One More Bro, I Swear
+// URL : https://codeforces.com/gym/105262/problem/J
+// Date: 4/12/2026
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    ll x; cin >> x;
+    cout <<x<<"\n";
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}// END: main.cpp
