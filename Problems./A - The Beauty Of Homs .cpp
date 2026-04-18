1afe97d6ec1080512a287ce882cbef24be4b87f1
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-04-18 11:47:57 +0200

    Create A - The Beauty Of Homs .cpp

---
+++ Problems./A - The Beauty Of Homs .cpp
@@ -0,0 +1,34 @@
+/**
+      A. The Beauty Of Homs 
+ *    author:  BetterCallUssif
+ *    created: 18.04.2026 17:18:32
+      URL : https://codeforces.com/gym/106016/problem/A
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    string x; cin >> x;
+    cout <<"Mara gebna gat tekarkar gebna qaletlha Kiri Kiri"<<"\n";
+}
+/*
+
+
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
