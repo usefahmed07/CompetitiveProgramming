d358208576a89c7f7476120a4c845ee6357fe0bf
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-04-18 11:38:27 +0200

    Create A - One Must Imagine Time Tight, .cpp

---
+++ Problems./A - One Must Imagine Time Tight, .cpp
@@ -0,0 +1,34 @@
+/**
+      A. One Must Imagine Time Tight,
+ *    author:  BetterCallUssif
+ *    created: 18.04.2026 17:18:32
+      URL : https://codeforces.com/gym/105902/problem/A
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    int a , b , c; cin >> a >> b >> c;
+    cout << min({a, b, c}) << "\n";
+}
+/*
+
+
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
