082ca8a92b1b041418565082cd7e01a697936dd6
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-04-19 21:43:24 +0200

    Create J. The Square Game.cpp

---
+++ Problems./J. The Square Game.cpp
@@ -0,0 +1,33 @@
+/**
+      J. The Square Game
+ *    author:  BetterCallUssif
+ *    created: 19.04.2026 17:18:32
+      URL : https://codeforces.com/gym/105242/problem/J
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    int x; cin >> x;
+    cout <<"Ahmad"<<"\n";
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+    return 0;
+}
