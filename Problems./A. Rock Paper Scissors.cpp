b5a6cf3db1333749040c93627f9f2f905ccae789
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-04-25 20:20:23 +0300

    Create A. Rock Paper Scissors.cpp

---
+++ Problems./A. Rock Paper Scissors.cpp
@@ -0,0 +1,41 @@
+/**
+      A. Rock Paper Scissors
+ *    author:  BetterCallUssif
+ *    created: 25.04.2026 17:18:32
+      URL : https://codeforces.com/group/WE2jmVU6Mi/contest/677810/problem/A
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+   string x , y; cin >> x >> y;
+   if (x == R && y == P){
+    cout <<"Mustafa"<<"\n";
+   }
+   else if (x == R && y == S){
+   cout <<"Delesseps"<<"\n";
+   }
+   else {
+    cout <<"Draw"<<"\n";
+   }
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
