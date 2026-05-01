0e5a1810624471ef06509c9fce37da2616899607
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-05-01 23:31:37 +0300

    Create K - University Exam .cpp

---
+++ Problems./K - University Exam .cpp
@@ -0,0 +1,46 @@
+/**
+      K. University Exam
+ *    author:  BetterCallUssif
+ *    created: 01.05.2026 17:18:32
+      URL : https://codeforces.com/gym/106032/problem/K
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    int a;
+    cin >> a;
+
+    if (a < 18) {
+        cout << "Failed\n";
+        return;
+    }
+
+    int b;
+    cin >> b;
+
+    if (a + b >= 60)
+        cout << "Passed\n";
+    else
+        cout << "Failed\n";
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
