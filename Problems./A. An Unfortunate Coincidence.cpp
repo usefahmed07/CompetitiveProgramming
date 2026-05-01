6a341e6929c2996da8030bd1af4487e211a4ebfa
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-05-01 19:30:00 +0300

    Create A. An Unfortunate Coincidence.cpp

---
+++ Problems./A. An Unfortunate Coincidence.cpp
@@ -0,0 +1,41 @@
+/**
+      A. An Unfortunate Coincidence
+ *    author:  BetterCallUssif
+ *    created: 01.05.2026 17:18:32
+      URL : https://codeforces.com/gym/106215/problem/A
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve (){
+    int tt; cin >> tt;
+    while (tt--){
+        string x; cin >> x;
+        if (x == "WY"){
+            cout <<"Whitney Young"<<"\n";
+        }
+        else {
+            cout << x <<"\n";
+        }
+    }
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
