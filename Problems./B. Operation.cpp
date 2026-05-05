df68b874e6d37d30f7ec5de05b43eabebd7ec183
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-05-05 18:23:27 +0300

    Create B. Operation.cpp

---
+++ Problems./B. Operation.cpp
@@ -0,0 +1,40 @@
+/**
+      B. Operation
+ *    author:  BetterCallUssif
+ *    created: 05.05.2026 
+      URL : https://codeforces.com/gym/104745/problem/B
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve (){
+    int a, b; cin >> a >> b;
+
+    double add = a + b;
+    double sub = a - b;
+    double mul = a * b;
+    double div = (double)a / b;
+
+    cout << max({add, sub, mul, div}) <<"\n";
+
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
