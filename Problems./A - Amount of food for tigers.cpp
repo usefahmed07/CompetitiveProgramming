913f274543a0bee316b9304b90a6e5816e793f3f
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-05-05 17:55:57 +0300

    Create A - Amount of food for tigers.cpp

---
+++ Problems./A - Amount of food for tigers.cpp
@@ -0,0 +1,38 @@
+/**
+      A. Amount of food for tigers
+ *    author:  BetterCallUssif
+ *    created: 05.05.2026 
+      URL : https://codeforces.com/gym/105904/problem/A
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve (){
+   int W , A , B , C; cin >> W >> A >> B >> C;
+   if (W >= A + B + C){
+    cout <<"S"<<"\n";
+   }
+   else {
+    cout <<"N"<<"\n";
+   }
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
