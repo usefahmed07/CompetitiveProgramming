55a8e2116df70e7d665730e5178a1c600d238f59
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-05-05 18:07:56 +0300

    Create A - Toy Trucks.cpp

---
+++ Problems./A - Toy Trucks.cpp
@@ -0,0 +1,38 @@
+/**
+      A. Toy Trucks
+ *    author:  BetterCallUssif
+ *    created: 05.05.2026 
+      URL : https://codeforces.com/gym/105023/problem/A
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve (){
+   int x; cin >> x;
+   if (x >= 1 && x <= 10){
+    cout <<"YES"<<"\n";
+   }
+   else {
+    cout <<"NO"<<"\n";
+   }
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
