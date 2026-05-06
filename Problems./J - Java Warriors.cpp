42a0f3919777f4ae477d104b1881c82321c11741
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-05-06 13:05:40 +0300

    Create J - Java Warriors.cpp

---
+++ Problems./J - Java Warriors.cpp
@@ -0,0 +1,33 @@
+/**
+      J. Java Warriors
+ *    author:  BetterCallUssif
+ *    created: 06.05.2026
+      URL : https://codeforces.com/gym/104619/problem/J
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve (){
+    int x; cin >> x;
+    cout << x * 4000 <<"\n";
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
