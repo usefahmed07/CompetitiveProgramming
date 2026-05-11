7a84b492e89f7b4b26524fbec4310141efcb13c3
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-05-11 18:05:44 +0300

    Create  I - Iron Fist Ketil vs King Canute.cpp

---
+++ Problems./I - Iron Fist Ketil vs King Canute.cpp
@@ -0,0 +1,38 @@
+/** 
+      I. Iron Fist Ketil vs King Canute
+ *    author:  BetterCallUssif
+ *    created: 11.05.2026
+      URL : https://codeforces.com/gym/104412/problem/I
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve (){
+   int x , y , z; cin >> x >> y >> z;
+   if (x / z >= y){
+    cout <<"Iron fist Ketil"<<"\n";
+   }
+   else {
+    cout <<"King Canute"<<"\n";
+   }
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
