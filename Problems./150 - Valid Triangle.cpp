245fa3b669a78b08efd994de09af0bf4a16f9ef7
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-05-12 02:18:55 +0300

    Create  150 - Valid Triangle.cpp

---
+++ Problems./150 - Valid Triangle.cpp
@@ -0,0 +1,38 @@
+/** 
+    150. Valid Triangle
+ *  author:  BetterCallUssif
+ *    created: 12.05.2026
+    URL : https://codeforces.com/gym/102697/problem/150
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve (){ 
+  int a , b , c; cin >> a >> b >> c;
+    if ((a + b > c) && (a + c > b) && (b + c > a)){
+    cout <<"VALID"<<"\n";
+  }
+  else {
+    cout <<"INVALID"<<"\n";
+  }
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
