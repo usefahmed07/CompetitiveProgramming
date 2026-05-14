a64ea0d1c181c37a7e27eb19b7505d4cdb1b3382
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-05-14 19:15:20 +0300

    Create 018 - Machines.cpp

---
+++ Problems./018 - Machines.cpp
@@ -0,0 +1,37 @@
+/**
+    018. Machines
+ *  author:  BetterCallUssif
+ *  created: 14.05.2026
+    URL : https://codeforces.com/gym/102697/problem/018
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+//cout << fixed << setprecision(16);
+void solve (){ 
+   int tt; cin >> tt;
+   cout << fixed << setprecision(16);
+   while (tt--){
+    double x , y; cin >> x >> y;
+    cout << x / y <<"\n";
+   }
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
