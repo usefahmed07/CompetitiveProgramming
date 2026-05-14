96eaa84ee320d1ba1b949f8e0b4bc269db6e1716
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-05-14 16:56:19 +0300

    Create 028 - Funny Numbers.cpp

---
+++ Problems./028 - Funny Numbers.cpp
@@ -0,0 +1,38 @@
+/**
+    028. Funny Numbers
+ *  author:  BetterCallUssif
+ *  created: 14.05.2026
+    URL : https://codeforces.com/gym/102697/problem/028
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve (){ 
+    int x; cin >> x;
+    if (x % 5 == 0 && x % 7 == 0){
+      cout <<"YES"<<"\n";
+    }
+    else {
+      cout <<"NO"<<"\n";
+    }
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
