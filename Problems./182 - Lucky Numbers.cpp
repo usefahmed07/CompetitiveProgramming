d04ee055d33fdbed95915f909f8bd34a97fe15e4
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-05-14 20:33:34 +0300

    Create 182 - Lucky Numbers.cpp

---
+++ Problems./182 - Lucky Numbers.cpp
@@ -0,0 +1,41 @@
+/**
+    182. Lucky Numbers
+ *  author:  BetterCallUssif
+ *  created: 14.05.2026
+    URL : https://codeforces.com/gym/102697/problem/182
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+//cout << fixed << setprecision(16);
+void solve (){ 
+    string n; cin >> n;
+
+    for (char c : n) {
+        if (c == '7') {
+            cout << "YES\n";
+            return 0;
+        }
+    }
+
+    cout << "NO\n";
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
