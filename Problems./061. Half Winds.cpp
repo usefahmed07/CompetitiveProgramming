c737d5177be0853d95f2117ee0bccc4e8d8a787d
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-05-16 00:22:19 +0300

    Add solution for problem 061. Half Winds

---
+++ Problems./061. Half Winds.cpp
@@ -0,0 +1,45 @@
+/**
+    061. Half Winds
+ *  author:  BetterCallUssif
+ *  created: 16.05.2026
+    URL : https://codeforces.com/gym/102697/problem/061
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+//cout << fixed << setprecision(16);
+void solve (){ 
+    string x; cin >> x;
+
+    map<string, string> mp = {
+        {"NNE", "north-northeast"},
+        {"ENE", "east-northeast"},
+        {"ESE", "east-southeast"},
+        {"SSE", "south-southeast"},
+        {"SSW", "south-southwest"},
+        {"WSW", "west-southwest"},
+        {"WNW", "west-northwest"},
+        {"NNW", "north-northwest"}
+    };
+
+    cout << mp[x] << "\n";
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
