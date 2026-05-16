f658e11dec3c35241fbcb0116575e0da5888d36b
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-05-16 14:58:12 +0300

    Create 058 - Switch Case.cpp

---
+++ Problems./058 - Switch Case.cpp
@@ -0,0 +1,41 @@
+/**
+    058. Switch Case
+ *  author:  BetterCallUssif
+ *  created: 16.05.2026
+    URL : https://codeforces.com/gym/102697/problem/058
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+//cout << fixed << setprecision(16);
+void solve (){
+    string s; cin >> s;
+
+    for (char &c : s) {
+        if (islower(c))
+            c = toupper(c);
+        else
+            c = tolower(c);
+    }
+
+    cout << s << endl;
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
