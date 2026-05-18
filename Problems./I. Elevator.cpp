358cc181d96b0b03c67e49e695b9ffdeefb1d050
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-05-18 15:10:24 +0300

    Create I. Elevator.cpp

---
+++ Problems./I. Elevator.cpp
@@ -0,0 +1,37 @@
+/**
+    I. Elevator
+ *  author:  BetterCallUssif
+ *  created: 18.05.2026
+    URL : https://codeforces.com/gym/104396/problem/I
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+//cout << fixed << setprecision(16);
+void solve (){ 
+    int t; cin >> t;
+    while (t--) {
+        int n, m; cin >> n >> m;
+        cout << n - (m - 1) << '\n';
+    }
+
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
