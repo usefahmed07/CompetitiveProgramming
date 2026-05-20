a321d44b8a00c9e985b5dd524433589db8ec019f
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-05-21 00:40:56 +0300

    Create  	A - My First Sorting Problem.cpp

---
+++ Problems./A - My First Sorting Problem.cpp
@@ -0,0 +1,50 @@
+/**
+    A. My First Sorting Problem
+ *  author:  BetterCallUssif
+ *  created: 21.05.2026
+    URL : https://codeforces.com/contest/1971/problem/A
+*
+*/
+// "Wake up to reality. Nothing ever goes as planned in this accursed world." – Madara Uchiha
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define F first
+#define S second
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define sz(x) (int)(x).size()
+#define mp(x, y) make_pair(x, y)
+#define popCnt(x) (__builtin_popcountll(x))
+#define int ll
+ 
+using ll = long long;
+using ii = pair<int, int>;
+using ull = unsigned long long;
+ 
+const int N = 2e6 + 5, A = 26, LG = 19, MOD = 1e9 + 7;
+const long double PI = acos(-1);
+const long double EPS = 1e-7;
+//cout << fixed << setprecision(16);
+void solve() {
+  int tt; cin >> tt;
+  while (tt--){
+    int a , b; cin >> a >> b;
+    cout << min(a , b) << " " << max(a , b) <<"\n";
+  }
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
