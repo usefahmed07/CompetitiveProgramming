7ed72db71744a983bec47156f0643a6917602699
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-06-23 12:33:12 +0300

    Create F. Lucky Numbe.cpp

---
+++ Problems./F. Lucky Numbe.cpp
@@ -0,0 +1,60 @@
+/** 
+    F. Lucky Number
+ *  author:  BetterCallUssif
+ *  created: 23.06.2026
+    URL : https://codeforces.com/group/x7tHVEYEUg/contest/238949/problem/F
+**/
+/// I am snatchin chains and burnin' tattoos
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define F first
+#define S second
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define sz(x) (int)(x).size()
+#define mp(x, y) make_pair(x, y)
+#define popCnt(x) (__builtin_popcountll(x))
+#define int ll
+ 
+using ll = long long;
+using ii = pair<int, int>;
+using ull = unsigned long long;
+ 
+const int N = 2e6 + 5, A = 26, LG = 19, MOD = 1e9 + 7;
+const long double PI = acos(-1);
+const long double EPS = 1e-7;
+// fixed << setprecision()
+void solve() {
+    int n; cin >> n;
+
+    int mn = 9, mx = 0;
+
+    while (n > 0) {
+        int d = n % 10;
+        mn = min(mn, d);
+        mx = max(mx, d);
+        n /= 10;
+    }
+
+    int diff = mx - mn;
+
+    if (diff == 4 || diff == 7)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr);
+    cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    // just normal comment for nonsense readers...
+    return 0;
+}
