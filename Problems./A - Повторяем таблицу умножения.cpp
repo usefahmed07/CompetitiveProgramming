a8b137f13bb36130cc5615b315645ea0a6acc788
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-06-25 15:54:41 +0300

    Create  A - Повторяем таблицу умножения.cpp

---
+++ Problems./A - Повторяем таблицу умножения.cpp
@@ -0,0 +1,48 @@
+/** 
+    A. Повторяем таблицу умножения
+ *  author:  BetterCallUssif
+ *  created: 25.06.2026
+    URL : https://codeforces.com/gym/106462/problem/A
+**/
+/// I am snatchin chains and burnin' tattoos
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define F first
+#define S second
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define sz(x) (int)(x).size()
+#define mp(x, y) make_pair(x, y)
+#define popCnt(x) (__builtin_popcountll(x))
+#define int ll
+ 
+using ll = long long;
+using ii = pair<int, int>;
+using ull = unsigned long long;
+ 
+const int N = 2e6 + 5, A = 26, LG = 19, MOD = 1e9 + 7;
+const long double PI = acos(-1);
+const long double EPS = 1e-7;
+// fixed << setprecision()
+void solve() {
+    int n; cin >> n;
+
+    for (int i = 1; i <= 10; i++) {
+        cout << n << " x " << i << " = " << n * i << '\n';
+    }
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr);
+    cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    // just normal comment for nonsense readers...
+    return 0;
+}
