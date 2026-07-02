d6d65c7a2e268438f47bffa977b726e55d7c7339
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-07-02 04:25:54 +0300

    Create I - Three Numbers Sum.cpp

---
+++ I - Three Numbers Sum.cpp
@@ -0,0 +1,57 @@
+/** 
+    I. Three Numbers Sum
+ *  author:  BetterCallUssif
+ *  created: 1.07.2026
+    URL : https://codeforces.com/group/j8QJucdRBd/contest/679131/problem/I
+**/
+/// I am snatchin chains and burnin' tattoos
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define F first
+#define S second
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define sz(x) (int)(x).size()
+#define mp(x, y) make_pair(x, y)
+#define popCnt(x) (__builtin_popcountll(x))
+#define int ll
+ 
+using ll = long long;
+using ii = pair<int, int>;
+using ull = unsigned long long;
+ 
+const int N = 2e6 + 5, A = 26, LG = 19, MOD = 1e9 + 7;
+const long double PI = acos(-1);
+const long double EPS = 1e-7;
+// fixed << setprecision()
+void solve() {
+    int K, S; cin >> K >> S;
+
+    int ans = 0;
+
+    for (int x = 0; x <= K; x++) {
+        for (int y = 0; y <= K; y++) {
+            int z = S - x - y;
+            if (z >= 0 && z <= K) {
+                ans++;
+            }
+        }
+    }
+
+    cout << ans << '\n';
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr);
+    cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    // just normal comment for nonsense readers...
+    return 0;
+}
