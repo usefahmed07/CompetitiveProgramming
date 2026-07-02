70a37fce365ed2cc294098f82f256ca92cee6b1b
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-07-02 04:13:21 +0300

    Create B - Guess who is the fool now.cpp

---
+++ Problems./B - Guess who is the fool now.cpp
@@ -0,0 +1,47 @@
+/** 
+    B. Guess who is the fool now
+ *  author:  BetterCallUssif
+ *  created: 1.07.2026
+    URL : https://codeforces.com/group/j8QJucdRBd/contest/676528/problem/B
+**/
+/// I am snatchin chains and burnin' tattoos
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define F first
+#define S second
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define sz(x) (int)(x).size()
+#define mp(x, y) make_pair(x, y)
+#define popCnt(x) (__builtin_popcountll(x))
+#define int ll
+ 
+using ll = long long;
+using ii = pair<int, int>;
+using ull = unsigned long long;
+ 
+const int N = 2e6 + 5, A = 26, LG = 19, MOD = 1e9 + 7;
+const long double PI = acos(-1);
+const long double EPS = 1e-7;
+// fixed << setprecision()
+void solve() {
+    int x; cin >> x;
+    for (int i = 0; i < x; i++){
+       cout <<"This problem is as useless as adham el saady"<<"\n";   
+    }
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr);
+    cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    // just normal comment for nonsense readers...
+    return 0;
+}
